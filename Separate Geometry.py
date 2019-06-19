7d4e03b9ebccca2ef5dcd5fd59aebd47de1a0a99
Tom <31204518+tomjerry1087@users.noreply.github.com>
2019-06-19 21:50:59 +0800

    Add files via upload

---
+++ Separate Geometry.py
@@ -0,0 +1,23 @@
+import toolutils
+scene_viewer=toolutils.sceneViewer()
+selected_objects=list(scene_viewer.selectObjects(
+	prompt=toolutils.selectionPrompt(hou.objNodeTypeCategory()),
+	allow_multisel=True,
+	allowed_types=("geo",)))
+if len(selected_objects)==0:
+	raise hou.Error("Nothing was selected.")
+for _obj in selected_objects:
+	_obj.setDisplayFlag(False)
+	dis_sop=_obj.displayNode()
+	conn_sop=dis_sop.createOutputNode('connectivity','connectivity',run_init_scripts=False)
+	conn_sop.parm('attribname').set('class')
+	part_sop=conn_sop.createOutputNode('partition','partition',run_init_scripts=False)
+	part_sop.parm('rule').set('`@class`')
+	part_sop.setDisplayFlag(True)
+	part_sop.setRenderFlag(True)
+	groups=part_sop.geometry().primGroups()
+	for group in groups:
+		new_obj=_obj.createOutputNode('geo',group.name(),run_init_scripts=False)
+		merge=new_obj.createNode('object_merge')
+		merge.parm('objpath1').set(part_sop.path())
+		merge.parm('group1').set(group.name())
